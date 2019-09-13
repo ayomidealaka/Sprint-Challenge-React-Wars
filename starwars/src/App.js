1053b1303e7ab3bc215a13bdbd084c1a8e8efaf0
vunderkind <justin.irabor@gmail.com>
2019-09-13 11:34:04 +0100

    Adding final changes

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,11 +9,12 @@ const App = () => {
     axios
     .get("https://swapi.co/api/people/")
     .then(response  => {
-      for (let key in response.data.results) {
-        setPeople(response.data.results[key])
-        console.log(response.data.results[key])
+    //   for (let key in response.data.results) {
+    //     setPeople(response.data.results[key])
+    //     console.log(key)
 
-    }
+    // }
+    setPeople(response.data.results)
     }, [])
   
     
@@ -22,11 +23,25 @@ const App = () => {
     })
   }, []);
 
+  
+  // Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+
   return (
     <div className="App">
+      
+
+        
+        {/* {console.log(People)} */}
+      
     <h1 className="Header">React Wars with</h1>
+    <h2>Name: {People.map(item=><p>{item.name}</p>)}</h2>
     </div>
   );
-
+}
 
 export default App;
